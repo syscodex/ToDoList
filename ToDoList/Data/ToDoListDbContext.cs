5c85d7be706ea00152b3b32d5541efd3342c0d05
Vincent <77495110+syscodex@users.noreply.github.com>
2024-11-24 00:09:32 +0800

    Refactor ToDoItem model and update migrations - Added length validation for 'Details' and 'AssignedTo' - Applied new migrations and ensured schema consistency

---
+++ ToDoList/Data/ToDoListDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
+
+namespace ToDoList.Data
+{
+    public class ToDoListDbContext : DbContext
+    {
+        public ToDoListDbContext(DbContextOptions<ToDoListDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+    }
+}
