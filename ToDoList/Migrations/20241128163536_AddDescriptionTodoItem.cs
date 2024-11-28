24cefee5b841fdade2a7ab5985849659ec4f2d6c
Vincent <77495110+syscodex@users.noreply.github.com>
2024-11-29 02:37:48 +0800

    Commit all changes: Updated models, improved task handling, and fixed layout issues
    - Added new properties and validation to the `ToDoItem` model.
    - Refined task filtering and helper functions.
    - Improved UI layout and responsiveness.

---
+++ ToDoList/Migrations/20241128163536_AddDescriptionTodoItem.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoList.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescriptionTodoItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "ToDoItems",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "ToDoItems");
+        }
+    }
+}
